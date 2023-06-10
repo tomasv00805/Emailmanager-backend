c1b07467c998e46cb7df8353f568f075ba231f67
tomas villalba buontempo <tomasv00805@gmail.com>
2023-06-10 02:21:10 -0300

    agregando clases

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import Mail from './Mail.js'
+import User from './User.js'
 const app = express()
 const port = process.env.PORT || 3000
 app.use(cors())
