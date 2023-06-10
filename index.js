2cc65dc662e821d1bbab3f2f06ccd990c0d146c5
tomas villalba buontempo <tomasv00805@gmail.com>
2023-06-10 02:16:43 -0300

    revivi porfa servidor

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import express from 'express';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import cors from 'cors';
 import Mail from './Mail.js';
 import User from './User.js';
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(cors())
 
-app.use(express.json())
+const express = require('express');
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 // Almacena los usuarios registrados y sus correos en arreglos
 const users = [
-  user1 = new User('tomi', 'tomi@gmail.com', '123'),
-  user2 = new User('juli', 'juli@gmail.com', '123'),
-  user3 = new User('joaqui', 'joaqui@gmail.com', '123'),
-  user4 = new User('gabriel', 'gabriel@gmail.com', '123'),
-  user5 = new User('facu', 'facu@gmail.com', '123')
-]
-const sentEmails = []
-const receivedEmails = []
+  new User('tomi', 'tomi@gmail.com', '123'),
+  new User('juli', 'juli@gmail.com', '123'),
+  new User('joaqui', 'joaqui@gmail.com', '123'),
+  new User('gabriel', 'gabriel@gmail.com', '123'),
+  new User('facu', 'facu@gmail.com', '123')
+];
+const sentEmails = [];
+const receivedEmails = [];
 
 // Ruta de registro de usuario
-app.post('/register', (req, res) => {
+app.post('/api/register', (req, res) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
@@ -41,7 +41,7 @@ app.post('/register', (req, res) => {
 });
 
 // Ruta de login de usuario
-app.post('/login', (req, res) => {
+app.post('/api/login', (req, res) => {
   const { identifier, password } = req.body;
 
   if (!identifier || !password) {
@@ -60,76 +60,81 @@ app.post('/login', (req, res) => {
 });
 
 // Ruta para enviar un correo
-app.post('/send', (req, res) => {
+app.post('/api/send', (req, res) => {
   const { from, to, subject, body } = req.body;
-  
+
   if (!from || !to || !subject || !body) {
-      return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+    return res.status(400).json({ error: 'Todos los campos son obligatorios' });
   }
-  
+
   const recipients = to.split(' ');
   const recipientIds = [];
-  
+
   // Verificar que todos los destinatarios (ya sea por username o email) existen
   recipients.forEach(recipient => {
-      const user = users.find(user => user.username === recipient || user.email === recipient);
-      if (!user) {
-          return res.status(401).json({ error: 'Usuario no registrado: ' + recipient });
-      }
-      recipientIds.push(user.username);
+    const user = users.find(user => user.username === recipient || user.email === recipient);
+    if (!user) {
+      return res.status(401).json({ error: 'Usuario no registrado: ' + recipient });
+    }
+    recipientIds.push(user.username);
   });
-  
-  const newEmail =new Mail(from, recipientIds, subject, body);
-  
+
+  const newEmail = new Mail(from, recipientIds, subject, body);
+
   sentEmails.push(newEmail);
   receivedEmails.push(newEmail);
-  
+
   return res.json({ message: 'Correo enviado' });
 });
 
 // Ruta para obtener la bandeja de entrada de un usuario
-app.get('/inbox/:username', (req, res) => {
-  const { username } = req.params
+app.get('/api/inbox/:username', (req, res) => {
+  const { username } = req.params;
 
   if (!username) {
-    return res.status(400).json({ error: 'Todos los campos son obligatorios' })
+    return res.status(400).json({ error: 'Todos los campos son obligatorios' });
   }
 
-  const user = users.find((user) => user.username === username)
+  const user = users.find((user) => user.username === username);
 
   if (!user) {
-    return res.status(401).json({ error: 'Usuario no registrado' })
+    return res.status(401).json({ error: 'Usuario no registrado' });
   }
 
-  const inbox = receivedEmails.filter((email) => email.to.includes(username))
+  const inbox = receivedEmails.filter((email) => email.to.includes(username));
 
-  return res.json(inbox)
-})
+  return res.json(inbox);
+});
 
 // Ruta para obtener la lista de correos enviados por un usuario
-app.get('/sent/:username', (req, res) => {
-  const { username } = req.params
+app.get('/api/sent/:username', (req, res) => {
+  const { username } = req.params;
 
   if (!username) {
-    return res.status(400).json({ error: 'Todos los campos son obligatorios' })
+    return res.status(400).json({ error: 'Todos los campos son obligatorios' });
   }
 
-  const user = users.find((user) => user.username === username)
+  const user = users.find((user) => user.username === username);
 
   if (!user) {
-    return res.status(401).json({ error: 'Usuario no registrado' })
+    return res.status(401).json({ error: 'Usuario no registrado' });
   }
 
-  const sent = sentEmails.filter((email) => email.from === username)
+  const sent = sentEmails.filter((email) => email.from === username);
 
-  return res.json(sent)
-})
+  return res.json(sent);
+});
 
-app.get('/allmails', (req, res) => {
-  return res.json(sentEmails)
-})
+app.get('/api/allmails', (req, res) => {
+  return res.json(sentEmails);
+});
 
+module.exports = (req, res) => {
+  if (req.method === 'OPTIONS') {
+    // Pre-flight request
+    res.status(200).end();
+    return;
+  }
 
-app.listen(port, () => {
-  console.log(`Servidor en ejecución en http://localhost:${port}`)
-})
+  app(req, res);
+};
