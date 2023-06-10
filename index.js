1651a2892390518ba71059d59690850ac428122a
tomas villalba buontempo <tomasv00805@gmail.com>
2023-06-10 02:38:33 -0300

    agregado email

--- index.js
+++ index.js
@@ -63,33 +63,36 @@ app.post('/login', (req, res) => {
 
 // Ruta para enviar un correo
 app.post('/send', (req, res) => {
-  const { from, to, subject, body } = req.body
-
-  if (!from || !to || !subject || !body) {
-    return res.status(400).json({ error: 'Todos los campos son obligatorios' })
-  }
-  const recipients = to.split(' ')
-  // recorrer arreglo recipients para verificar que todos los usuarios existen
-  const allRecipientsExist = recipients.every((recipient) =>
-    users.some((user) => user.username === recipient)
-  )
-
-  if (!allRecipientsExist) {
-    return res.status(401).json({ error: 'Usuario no registrado' })
-  }
-
-  const newEmail = {
-    from,
-    to: recipients,
-    subject,
-    body
-  }
-
-  sentEmails.push(newEmail)
-  receivedEmails.push(newEmail)
-
-  return res.json({ message: 'Correo enviado' })
-})
+    const { from, to, subject, body } = req.body;
+    
+    if (!from || !to || !subject || !body) {
+        return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+    }
+    
+    const recipients = to.split(' ');
+    const recipientIds = [];
+    
+    // Verificar que todos los destinatarios (ya sea por username o email) existen
+    recipients.forEach(recipient => {
+        const user = users.find(user => user.username === recipient || user.email === recipient);
+        if (!user) {
+            return res.status(401).json({ error: 'Usuario no registrado: ' + recipient });
+        }
+        recipientIds.push(user.username);
+    });
+    
+    const newEmail = {
+        from,
+        to: recipientIds,
+        subject,
+        body
+    };
+    
+    sentEmails.push(newEmail);
+    receivedEmails.push(newEmail);
+    
+    return res.json({ message: 'Correo enviado' });
+});
 
 // Ruta para obtener la bandeja de entrada de un usuario
 app.get('/inbox/:username', (req, res) => {
@@ -131,24 +134,6 @@ app.get('/sent/:username', (req, res) => {
 
 app.get('/allmails', (req, res) => {
   return res.json(sentEmails)
-})
-
-app.get('/keep-alive', (req, res) => {
-  res.send('Backend is alive!')
-})
-
-// Configurar el intervalo de tiempo para el mecanismo de "ping"
-const keepAliveInterval = setInterval(() => {
-  fetch('https://tu-backend.vercel.app/keep-alive')
-    .then((response) => response.text())
-    .then(console.log)
-    .catch(console.error)
-}, 300000) // Cada 5 minutos (300000 milisegundos)
-
-// Detener el mecanismo de "ping" al cerrar la aplicación
-process.on('SIGINT', () => {
-  clearInterval(keepAliveInterval)
-  process.exit()
 })
 
 app.listen(port, () => {
