9188655d2a1879474f63e251ed9b5f02a0328015
tomas villalba buontempo <tomasv00805@gmail.com>
2023-06-10 02:47:42 -0300

    agregado email

--- class/User.js
+++ class/User.js
@@ -1,9 +1,10 @@
 //creame una clase usuario que los atributos sean username, email y password
 
-class Usuario {
+class User {
   constructor(username, email, password) {
     this.username = username;
     this.email = email;
     this.password = password;
   }
 }
+module.exports = User;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+const User = require('./class/User');
 const app = express()
 const port = process.env.PORT || 3000
 app.use(cors())
