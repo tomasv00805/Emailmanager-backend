3608a12407fb41a134c63e06bae7499e77489d8c
tomas villalba buontempo <tomasv00805@gmail.com>
2023-06-10 02:05:01 -0300

    arreglando usuers

---
+++ Mail.js
@@ -0,0 +1,10 @@
+//crear una clase mail donde contenga un from, to, subject y un body
+
+class Mail {
+    constructor(from, to, subject, body) {
+        this.from = from;
+        this.to = to;
+        this.subject = subject;
+        this.body = body;
+    }
+}
---
+++ User.js
@@ -0,0 +1,9 @@
+//creame una clase usuario que los atributos sean username, email y password
+
+class Usuario {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+}
