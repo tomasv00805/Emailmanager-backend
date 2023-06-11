1d9db1988bf1768ce0d1f7ba84f32352c44cba15
tomas villalba buontempo <tomasv00805@gmail.com>
2023-06-11 02:34:53 -0300

    id y fav en proceso

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3000
 app.use(cors())
-
+let nextEmailId = 1;
 app.use(express.json())
 
 // Almacena los usuarios registrados y sus correos en arreglos
 const users = [
-  { username: 'tomi', email: "tomi@mail.com", password: '123' },
-  { username: 'juli', email: "juli@mail.com",password: '123' },
-  { username: 'joaqui', email: "joaqui@mail.com",password: '123' },
-  { username: 'gabriel',email: "gabriel@mail.com" , password: '123' },
-  { username: 'facu', email: "facu@mail.com",password: '123' }
+  { username: 'tomi', email: "tomi@mail.com", password: '123',fav:[] },
+  { username: 'juli', email: "juli@mail.com",password: '123', fav:[] },
+  { username: 'joaqui', email: "joaqui@mail.com",password: '123', fav:[] },
+  { username: 'gabriel',email: "gabriel@mail.com" , password: '123', fav:[] },
+  { username: 'facu', email: "facu@mail.com",password: '123', fav:[] }
 ]
 const sentEmails = []
 const receivedEmails = []
@@ -63,35 +63,39 @@ app.post('/login', (req, res) => {
 
 // Ruta para enviar un correo
 app.post('/send', (req, res) => {
-    const { from, to, subject, body } = req.body;
-    
-    if (!from || !to || !subject || !body) {
-        return res.status(400).json({ error: 'Todos los campos son obligatorios' });
-    }
-    
-    const recipients = to.split(' ');
-    const recipientIds = [];
-    
-    // Verificar que todos los destinatarios (ya sea por username o email) existen
-    recipients.forEach(recipient => {
-        const user = users.find(user => user.username === recipient || user.email === recipient);
-        if (!user) {
-            return res.status(401).json({ error: 'Usuario no registrado: ' + recipient });
-        }
-        recipientIds.push(user.username);
-    });
-    
-    const newEmail = {
-        from,
-        to: recipientIds,
-        subject,
-        body
-    };
-    
-    sentEmails.push(newEmail);
-    receivedEmails.push(newEmail);
-    
-    return res.json({ message: 'Correo enviado' });
+  const { from, to, subject, body } = req.body;
+  
+  if (!from || !to || !subject || !body) {
+      return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+  }
+  
+  const recipients = to.split(' ');
+  const recipientIds = [];
+  
+  // Verificar que todos los destinatarios (ya sea por username o email) existen
+  recipients.forEach(recipient => {
+      const user = users.find(user => user.username === recipient || user.email === recipient);
+      if (!user) {
+          return res.status(401).json({ error: 'Usuario no registrado: ' + recipient });
+      }
+      recipientIds.push(user.username);
+  });
+  
+  const newEmail = {
+      id: nextEmailId,
+      from,
+      to: recipientIds,
+      subject,
+      body
+  };
+  
+  sentEmails.push(newEmail);
+  receivedEmails.push(newEmail);
+
+  // Incrementar el próximo ID de correo disponible
+  nextEmailId++;
+  
+  return res.json({ message: 'Correo enviado' });
 });
 
 // Ruta para obtener la bandeja de entrada de un usuario
@@ -121,7 +125,7 @@ app.get('/sent/:username', (req, res) => {
     return res.status(400).json({ error: 'Todos los campos son obligatorios' })
   }
 
-  const user = users.find((user) => user.username === username)
+  const user = users.find((user) => user.username === username || user.email === username)
 
   if (!user) {
     return res.status(401).json({ error: 'Usuario no registrado' })
@@ -135,6 +139,55 @@ app.get('/sent/:username', (req, res) => {
 app.get('/allmails', (req, res) => {
   return res.json(sentEmails)
 })
+
+// ruta para guardar en favoritos de un usuario
+app.post('/favorite/:username', (req, res) => {
+  const { username } = req.params;
+  const { emailId } = req.body;
+
+  if (!username || !emailId) {
+    return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+  }
+
+  const user = users.find((user) => user.username === username || user.email === username);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Usuario no registrado' });
+  }
+
+  const email = sentEmails.find((email) => email.id === emailId);
+
+  if (!email) {
+    return res.status(404).json({ error: 'Correo no encontrado' });
+  }
+
+  if (user.fav.includes(emailId)) {
+    return res.status(409).json({ error: 'El correo ya está en favoritos' });
+  }
+
+  user.fav.push(emailId);
+
+  return res.json({ message: 'Correo agregado a favoritos' });
+});
+// Ruta para obtener la lista de correos favoritos de un usuario
+app.get('/favorites/:username', (req, res) => {
+  const { username } = req.params;
+
+  if (!username) {
+    return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+  }
+
+  const user = users.find((user) => user.username === username || user.email === username);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Usuario no registrado' });
+  }
+
+  const favoriteEmails = sentEmails.filter((email) => user.fav.includes(email.id));
+
+  return res.json(favoriteEmails);
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor en ejecución en http://localhost:${port}`)
